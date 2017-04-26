13760103012b1fe614578388572816233e802d75
LCJA <liyq1004@163.com>
2017-04-26 14:43:40 +0800

    修改表

--- src/main/resources/sql/fp_count.sql
+++ src/main/resources/sql/fp_count.sql
@@ -4,9 +4,11 @@
   btn_type  varchar(20) NOT NULL,
   create_date date NOT NULL,
   state int(5),
-  ext1 varchar(40),
+  ext1 bigint(20),
   ext2 varchar(40),
    ext3 varchar(40),
    ext4 varchar(40),
    ext5 varchar(40)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+alter table fp_count alter column ext1 set default 0;
